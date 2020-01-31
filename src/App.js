6795f6e723aa39b91ea80ede1d22b7f8473e3cc1
Neel <krishlal925@gmail.com>
2020-01-30 20:42:22 -0800

    showing numer of notes

--- src/App.js
+++ src/App.js
@@ -42,24 +42,49 @@ console.log(user.email)
   );
 }
 
+function Notes ({notes}){
+  return (
+    <div>
+      <h3>Notes</h3>
+      <p>You have {notes.length} notes.</p>
+    </div>
+  );
+}
 
+async function getNotes(user){
+  let notes = await axios.get(`${API}/users/${user.id}/notes`)
+  return notes;
+}
  function App() {
   const [user, setUser] = useState('')
+  const [notes, setNotes] = useState('')
 
   useEffect(() => {
     fetchUser()
     .then((user) => setUser(user))
   }, [])
 
+  useEffect(() =>{
+    getNotes(user)
+      .then((notes)=> setNotes(notes.data))
+  },[user])
+
+
   const changeUser = () =>{
     window.localStorage.removeItem('userId')
     fetchUser()
       .then((user)=> setUser(user) )
   }
+
+
     return (
       <div className="App">
         <Nav user = {user} changeUser = {changeUser}/>
+        <div className= "main">
+          <Notes notes = {notes}/>
+        </div>
       </div>
+
     );
 }
 
